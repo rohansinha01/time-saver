c9aac6f608be0a88bd963bb3b605f204e8b7d2a7
rohansinha01 <rohansinha01@gmail.com>
2025-03-06 20:48:26 -0500

    passed props

--- app/Components/Toggle.tsx
+++ app/Components/Toggle.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Toggle = () => {
+interface ToggleProps {
+  label: string;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ label }) => {
   return (
-    <div>
-        <label>
-         <input />    
-         <span />
+    <div className='p-8'>
+        {label}
+        <label htmlFor='check' className='flex bg-gray-100 cursor-pointer relative w-20 h-10 rounded-full'>
+         <input type='checkbox' id="check" className='sr-only peer'/>    
+         <span className='w-2/5 h-4/5 bg-rose-300 absolute rounded-full left-1 top-1 peer-checked:bg-rose-600 peer-checked:left-11 transition-all duration-500'/>
         </label>
        
         
